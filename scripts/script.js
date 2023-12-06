c348c308643f32bbfaf33d595dd1edca0da162c7
OvenMagnetron <jessekramer15@gmail.com>
2023-12-06 12:09:15 +0100

    Af

--- scripts/script.js
+++ scripts/script.js
@@ -4,10 +4,10 @@ const body = document.querySelector('body'),
     searchBtn = body.querySelector(".search-box");
 
 const introContainer = document.getElementById("introContainer");
-const searchForm = document.getElementById("searchForm");
 const resultsContainer = document.getElementById("resultsContainer");
 const nieuwsbriefContainer = document.getElementById("nieuwsbriefContainer");
 const toggleButton = document.getElementById("toggleButton");
+const searchInput = document.querySelector(".input-field");
 
 toggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
@@ -17,15 +17,24 @@ searchBtn.addEventListener("click", () => {
     sidebar.classList.remove("close");
 })
 
-searchForm.addEventListener("focus", () => {
+searchInput.addEventListener("focus", () => {
     introContainer.classList.add("hidden");
     resultsContainer.classList.remove("hidden");
+    toggleButton.classList.remove("hidden");
     nieuwsbriefContainer.classList.add("hidden");
 });
 
 toggleButton.addEventListener("click", () => {
     introContainer.classList.remove("hidden");
     resultsContainer.classList.add("hidden");
+    toggleButton.classList.add("hidden");
     nieuwsbriefContainer.classList.remove("hidden");
 })
 
+searchInput.addEventListener("input", () => {
+    if (searchInput.value.trim() !== "") {
+        searchInput.classList.add("focused");
+    } else {
+        searchInput.classList.remove("focused");
+    }
+});
